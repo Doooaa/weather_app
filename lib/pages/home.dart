72841aaa1645565726cb9069202a623c91af0508
Doooaa <d98744330@gmail.com>
2023-07-04 18:27:13 +0300

    display data

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,83 +1,126 @@
+import 'dart:ui';
 import 'package:flutter/material.dart';
 import 'package:weathr_app/weather_model.dart';
 import 'package:weathr_app/pages/search_page.dart';
 
-class home_page extends StatelessWidget {
-   home_page({super.key});
-   weatherModel? weatherData;
+
+class home_page extends StatefulWidget {
+  home_page({super.key});
+
+  @override
+  State<home_page> createState() => _home_pageState();
+}
+
+class _home_pageState extends State<home_page> {
+  //weatherModel? weatherData;
+  void updateUi() {
+    setState(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
-    return weatherData!=null?Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Center(
-            child: Row(
-              children: [
-                Expanded(
-                  child: Text(
-                    "There is no weather data. Please search for a specified city!",
-                    textAlign: TextAlign.center,
-                  ),
-                ),
-                Expanded(
-                  child: IconButton(
-                    onPressed: () {
-                      Navigator.push(context, 
-                      MaterialPageRoute(builder: (context) => SearchPage()),);
-                    },
-                    icon: Icon(Icons.search),
-                  ),
-                ),
-              ],
+    return weatherData == null
+        ? Scaffold(
+            appBar: AppBar(
+              title: Text('hooooooom'),
             ),
-          )
-        ],
-      ),
-    ):Container(
-      child:  Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-            Spacer(flex: 3,),
-           Text("cario",style: TextStyle(
-             fontSize: 27,
-             fontWeight: FontWeight.bold,
-           ),),
-            Text("2023-7-2",style: TextStyle(
-             fontSize: 12,
-             fontWeight: FontWeight.bold,
-           ),),
-           Spacer(),
-            Row(
-              children: [
-              
-             Expanded(
-               child: Image.network(
-                         'https://media.istockphoto.com/id/1218939051/photo/asian-model-in-the-water-enjoying-a-wellness-spa.jpg?s=2048x2048&w=is&k=20&c=lkBPElSUEOhwY6O5Bwe0-Emgc27WdTC7wApMgUIL45Y=', // Replace with your image URL
-                         loadingBuilder: (context, child, progress) {
-                if (progress == null) {
-                  return child;
-                }
-                return CircularProgressIndicator();
-                         },
-                         errorBuilder: (context, error, stackTrace) {
-                return Text('Error loading image');
-                         },
-                       ),
-             ),
-                 Expanded(child: Text('30')),
-                 Expanded(child: Text('30')),
-                 Expanded(child: Text('30')),
-               
+            body:
+              //.......................................background..........
 
-              ],
+               Column(
+                
+                
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Center(
+                    child: Row(
+                      children: [
+                        Expanded(
+                          child: Text(
+                            "There is no weather data. Please search for a specified city!",
+                            textAlign: TextAlign.center,
+                          ),
+                        ),
+                        Expanded(
+                          child: IconButton(
+                            onPressed: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => SearchPage(
+                                          updateUi: updateUi,
+                                        )),
+                              );
+                            },
+                            icon: Icon(Icons.search),
+                          ),
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            
+        )
+        : Scaffold(
+            body: Container(
+              color: Colors.blue,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Spacer(
+                    flex: 3,
+                  ),
+                  Text(
+                    '${weatherData!.cityName.toString()}',
+                    style: TextStyle(
+                      fontSize: 27,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Text(
+                    "${weatherData!.date.toString()}",
+                    style: TextStyle(
+                      fontSize: 12,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Spacer(),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Expanded(
+                        child: Image.network(
+                          '${'https:' + weatherData!.imageState.toString()}',
+                          loadingBuilder: (context, child, progress) {
+                            if (progress == null) {
+                              return child;
+                            }
+                            return CircularProgressIndicator();
+                          },
+                          errorBuilder: (context, error, stackTrace) {
+                            return Text('Error loading image');
+                          },
+                        ),
+                      ),
+                      Expanded(
+                          child: Text('${weatherData!.av_temp.toString()}')),
+                      Column(children: [
+                        Text(
+                            'max temprture :${weatherData!.max_temp.toString()}'),
+                        Text(
+                            'min temprture :${weatherData!.min_temp.toString()}')
+                      ]),
+                    ],
+                  ),
+                  Spacer(),
+                  Text('${weatherData!.state.toString()}'),
+                  Spacer(
+                    flex: 5,
+                  ),
+                ],
+              ),
             ),
-            Spacer(),
-            Text('endddddddddd'),
-            Spacer(flex: 3,),
-        ],
-      ),
-
-    );
+          );
   }
 }
